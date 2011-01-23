6e9b9dda76435983a5a762aa2c4d960696da2448
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2011-01-22 23:37:20 -0500

    moved nav_currentid() to common_currentid() in turnip_utils.php; commented turnip_utils.php to hell (this will all become irrelevant if I do it object-oriented like I should but WHATEVER)

--- turnip.php
+++ turnip.php
@@ -8,7 +8,7 @@ function displaycomic($id = 'current')
 {
 
 	if ($id=='current'){
-            $current = nav_currentid();
+            $current = common_currentid();
         } else {
            $current = $id;
         }
@@ -16,43 +16,6 @@ function displaycomic($id = 'current')
 }
 
 // COMIC NAVIGATION
-
-function nav_currentid()
-{
-$link = mysql_connect(common_config('database','host'),
-    common_config('database','user'),
-    common_config('database','password'));
-
-if (!$link)
-{
-    header('HTTP/1.0 500 Internal Server Error');
-    exit;
-}
-
-if (!mysql_select_db(common_config('database','name')))
-{
-    header('HTTP/1.0 500 Internal Server Error');
-    exit;
-}
-$query = "SELECT id FROM comic WHERE date <= '" . common_sql_date(time()) . "'ORDER BY id DESC LIMIT 1;";
-$result = mysql_query($query);
-
-if(!$result)
-{
-    header('HTTP/1.0 500 Internal Server Error');
-    exit;
-}
-
-if (mysql_num_rows($result) == 0)
-{
-    header('HTTP/1.0 500 Internal Server Error');
-    exit;
-}
-
-$line = mysql_fetch_assoc($result);
-mysql_free_result($result);
-return $line['id'];
-}
 
 //first
 function nav_first($id = 'current')
@@ -64,7 +27,7 @@ function nav_first($id = 'current')
 function nav_prev($id = 'current')
 {
     if ($id=='current'){
-        $current = nav_currentid();
+        $current = common_currentid();
     } else {
         $current = $id;
     }
@@ -84,7 +47,7 @@ function nav_next($id = 'current')
         return;
     }
 
-    if($id==nav_currentid())
+    if($id==common_currentid())
     {
         echo '#';
     } else {
@@ -104,7 +67,7 @@ function nav_last($id = 'current')
 function newspost($id = 'current')
 {
 	if ($id=='current'){
-            $current = nav_currentid();
+            $current = common_currentid();
         } else {
            $current = $id;
         }
--- turnip_utils.php
+++ turnip_utils.php
@@ -5,21 +5,86 @@ define('TURNIPDIR', dirname(__FILE__));
 
 include(TURNIPDIR . '/turnip_config.php');
 
-function common_config($main,$sub)
+/***********************************
+ * Return a config value from the  *
+ * category and property it's      *
+ * stored under.                   *
+ *                                 *
+ * Optional third argument is a    *
+ * default to return if a property *
+ * is not found.                   *
+ ***********************************
+ */
+function common_config($main,$sub,$default = false)
 {
     global $config;
     return (array_key_exists($main,$config) && 
-            array_key_exists($sub,$config[$main])) ? $config[$main][$sub] : false;
+            array_key_exists($sub,$config[$main])) ? $config[$main][$sub] : $default;
 }
 
+/*******************************
+ * Returns an SQL time string  *
+ * from a PHP time object      *
+ *******************************
+ */
 function common_sql_date($datetime)
 {
     return strftime('%Y-%m-%d',$datetime);
 }
 
-function common_php_date($datetime)
+/*******************************
+ * Returns a PHP time object   *
+ * from the format used in SQL *
+ *******************************
+ */
+function common_php_date($timestr)
 {
-    return strtotime($datetime);
+    return strtotime($timestr);
+}
+
+/******************************
+ * Returns the ID number of   *
+ * the current comic.         *
+ * (that is, the one with the *
+ * highest ID whose release   *
+ * date has passed)           *
+ ******************************
+ */
+function common_currentid()
+{
+    $link = mysql_connect(common_config('database','host'),
+        common_config('database','user'),
+        common_config('database','password'));
+
+    if (!$link)
+    {
+        header('HTTP/1.0 500 Internal Server Error');
+        exit;
+    }
+
+    if (!mysql_select_db(common_config('database','name')))
+    {
+        header('HTTP/1.0 500 Internal Server Error');
+        exit;
+    }
+    $query = "SELECT id FROM comic WHERE date <= '" . common_sql_date(time()) . "'ORDER BY id DESC LIMIT 1;";
+    $result = mysql_query($query);
+
+    if(!$result)
+    {
+        header('HTTP/1.0 500 Internal Server Error');
+        exit;
+    }
+
+    if (mysql_num_rows($result) == 0)
+    {
+        header('HTTP/1.0 500 Internal Server Error');
+        exit;
+    }
+
+    $line = mysql_fetch_assoc($result);
+    mysql_free_result($result);
+    return $line['id'];
 }
 
 ?>
