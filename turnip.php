30e11ed27fb6eee6a8a62ae3d1e3abf0da3dfc77
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2011-01-23 00:19:55 -0500

    Hoping and presuming it works, object-orientation! Resultingly, fewer SQL hits. Next: making it work with more than MySQL?

--- turnip.php
+++ turnip.php
@@ -4,101 +4,139 @@ include('turnip_utils.php');
 
 // DISPLAY COMICS
 
-function displaycomic($id = 'current')
+class Comic
 {
+    public $id = 'current';
+    public $current = null;
+    public $newspost = null;
+    public $alt_title = null;
 
-	if ($id=='current'){
-            $current = common_currentid();
-        } else {
-           $current = $id;
-        }
-        printf(common_config('comic','location'), $current);
-}
-
-// COMIC NAVIGATION
-
-//first
-function nav_first($id = 'current')
-{
-    printf(common_config('comic','previous'), 1);
-}
-
-//previous
-function nav_prev($id = 'current')
-{
-    if ($id=='current'){
-        $current = common_currentid();
-    } else {
-        $current = $id;
-    }
-    if ($current==1){
-        echo '#';
-    } else {
-        printf(common_config('comic','previous'), $current-1);
-    }
-}
-
-//next
-function nav_next($id = 'current')
-{
-    if ($id == 'current')
+    function __construct($id = 'current')
     {
-        echo '#';
-        return;
+        $this->id = $id;
+        $this->current = (int) common_currentid();
+        $this->resolve_id();
     }
-
-    if($id==common_currentid())
+    
+    /*******************************************
+     * If $this->id is still set to 'current', *
+     * change it to the current comic ID.      *
+     *******************************************/
+    public function resolve_id()
     {
-        echo '#';
-    } else {
-        printf(common_config('comic','previous'), $id+1);
-    }
-}
-
-//current
-function nav_last($id = 'current')
-{
-    echo common_config('comic','current');
-}
-
-
-
-// NEWSPOSTS
-function newspost($id = 'current')
-{
-	if ($id=='current'){
-            $current = common_currentid();
+        if ($this->id=='current') {
+            $this->id = (int) $this->current;
         } else {
-           $current = $id;
+            $this->id = (int) $this->id;
+        }
+    }
+
+    // COMIC DISPLAY
+
+    /***************************************
+     * Print a URL to be used in an img    *
+     * src attribute, to display the comic *
+     ***************************************/
+    public function display()
+    {
+        printf(common_config('comic', 'location'), $this->id);
+    }
+
+    // COMIC NAVIGATION
+
+    /*************************************
+     * Print URLs for a href attributes: *
+     * first, previous, next, and        *
+     * last/current comic links          *
+     *************************************/
+    public function nav_first()
+    {
+        printf(common_config('comic','previous'), 1);
+    }
+
+    public function nav_prev()
+    {
+        if ($this->id == 1) {
+            echo '#';
+        } else {
+            printf(common_config('comic', 'previous'), $this->id-1);
+        }
+    }
+
+    public function nav_next()
+    {
+        if($this->id == $this->current)
+        {
+            echo '#';
+        } else {
+            printf(common_config('comic','previous'), $this->id+1);
+        }
+    }
+
+    public function nav_last()
+    {
+        if (common_config('comic', 'use index for current', true)) {
+            echo common_config('comic','current');
+        } else {
+            printf(common_config('comic','previous'), $this->current);
+        }
+    }
+    
+    // NEWSPOSTS AND ALT/TITLE TEXT
+
+    /***********************************
+     * Print me my newspost, kind sir! *
+     ***********************************/
+    public function newspost()
+    {
+        if (is_null($this->newspost))
+        {
+            $this->fetch_newspost_table();
+        }
+        echo $this->newspost;
+    }
+
+    public function alt_title()
+    {
+        if (is_null($this->alt_title))
+        {
+            $this->fetch_newspost_table();
+        }
+        echo $this->alt_title;
+    }
+
+    /********************************
+     * If you don't already got it, *
+     * get it from the database!    *
+     ********************************/
+    public function fetch_newspost_table()
+    {
+        $link = mysql_connect(common_config('database','host'),
+            common_config('database','user'),
+            common_config('database','password'));
+
+        // debug log?
+        if (!$link) { exit; }
+        if (!mysql_select_db(common_config('database','name'))) { exit; }
+
+        $query = "SELECT newspost, alt_title FROM newspost WHERE id = $current";
+        $result = mysql_query($query);
+
+        // debug log? 
+        if(!$result) { exit; }
+
+        if(mysql_num_rows($result) == 0)
+        {
+            $this->newspost = '';
+            $this->alt_title = '';
+        } else {
+            $line = mysql_fetch_assoc($result);
+            $this->newspost = stripslashes($line['newspost']);
+            $this->alt_title = stripslashes($line['alt_title']);
         }
 
-$link = mysql_connect(common_config('database','host'),
-    common_config('database','user'),
-    common_config('database','password'));
-
-if (!$link)
-{
-    header('HTTP/1.0 500 Internal Server Error');
-    exit;
-}
-
-if (!mysql_select_db(common_config('database','name')))
-{
-    header('HTTP/1.0 500 Internal Server Error');
-    exit;
-}
-$query = "SELECT newspost FROM newspost WHERE id = $current";
-$result = mysql_query($query);
-
-if(!$result)
-{
-    header('HTTP/1.0 500 Internal Server Error');
-    exit;
-}
-
-$line = mysql_fetch_assoc($result);
-echo stripslashes($line['newspost']);
-mysql_free_result($result);
+        mysql_free_result($result);
+    }
 }
 
 ?>
